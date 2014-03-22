91eb741133eff118f20170af3ec3b58ee49c54ca
Gregory Swanson <gregcswanson@gmail.com>
2014-03-22 23:50:17 +0000

    initial checkin

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ fuzzy-hipster
 =============
 
 Check and Recheck web application
+
+Launch from Nitrious.IO
+dev_appserver.py --address=0.0.0.0 --port=3000 workspace/fuzzy-hipster/
---
+++ fuzzyhipster/fuzzyhipster.go
@@ -0,0 +1,16 @@
+package fuzzyhipster
+
+import (
+    "fmt"
+    "net/http"
+)
+
+
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world!")
+}
